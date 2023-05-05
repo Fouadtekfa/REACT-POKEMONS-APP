c588620cf221e1a91859155d60092cafbeab0710
TEKFA FOUAD <fouad.tekfa@etu.univ-lehavre.fr>
2023-05-06 01:57:32 +0200

    ajout de models pokemons && afichage de nombre totale des pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React,{FunctionComponent} from 'react';
+import React,{FunctionComponent,useState} from 'react';
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name: String = 'React';
+//    const name: String = 'React';
+    //etat init
+    const [pokemons,setName]=useState<Pokemon[]>(POKEMONS);
 
     return (
-        <h1>Hello, {name} !</h1>
+        <div>
+         <h1>Pokédex</h1>
+         <p>Ii ya {pokemons.length} POKEMONS dans le pokédex</p>
+        </div>
+
     )
 }
 
