66fa600eb52759d836b9b040899f94634ae936e8
TEKFA FOUAD <fouad.tekfa@etu.univ-lehavre.fr>
2023-05-05 22:11:31 +0200

    mettre en place l'architecteure de projet

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+//cette methode va prendre deux paramètres un composant react et un élément html avec le id root
+ReactDOM.render(
+  <App/>,
+    document.getElementById('root')
+
+);
