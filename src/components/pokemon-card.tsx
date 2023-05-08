e086bf8b2f377ef5764a56aed4fd31a2acded91f
TEKFA FOUAD <fouad.tekfa@etu.univ-lehavre.fr>
2023-05-09 01:22:13 +0200

    factoriser le comportement commun entre plusieurs composants

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent,useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-type";
 
 type Props = {
     pokemon: Pokemon,
@@ -15,6 +17,8 @@ const showBorder =()=>{
     const hideBorder =()=>{
         setColor("#f5f5f5");
     }
+
+
     return (
         <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
             <div className="card horizontal" style={{borderColor: color}}>
@@ -24,7 +28,11 @@ const showBorder =()=>{
                 <div className="card-stacked">
                     <div className="card-content">
                         <p>{pokemon.name}</p>
-                        <p><small>{pokemon.created.toString()}</small></p>
+                        <p><small>{formatDate(pokemon.created)}</small></p>
+                        {pokemon.types.map(type=>(
+                            <span key={type} className={formatType(type)}>{type}</span>
+                        ))}
+
                     </div>
                 </div>
             </div>
